398c46f770296c51de236f0f5d6e343f77ddbc05
BertraMoon <bertramoon@126.com>
2021-03-27 21:45:10 +0800

    generate v0.0.6

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="dgut_requests",
-    version="0.0.5",
+    version="0.0.6",
     author="Bertramoon",
     author_email="bertramoon@126.com",
     description="用requests等库封装的东莞理工学院相关系统的爬虫脚本库",
